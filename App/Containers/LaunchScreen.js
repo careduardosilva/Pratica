97d5dfebd60fbb69b505e25f0e80ba361d141768
Carlos <aryella.lacerda@gmail.com>
2019-03-25 13:49:31 -0300

    new  commit

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,103 +1,58 @@
-import React, { Component } from 'react'; 
-import { TouchableHighlight, Text, View } from 'react-native';
-import { Button } from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, TextInput, Button, YellowBox} from 'react-native';
+import { Directions } from 'react-native-gesture-handler';
+import styles from './Styles/LaunchScreenStyles'
 
-export default class Contador extends Component {
+class App extends Component {
   state = {
-    segundos1: 0,
-    segundos2: 0,
-    minutos1: 0,
-    minutos2: 0,
-    horas1: 0,
-    horas2: 0,
-    bug: false,
+    login: '',
+    password: '',
   }
-
-  time = () => {
-    console.log(this.state.bug)
-    alert(this.state.bug)
-    if(this.state.bug == false){
-        this.Interval = setInterval(() => {
-          this.setState({
-          segundos1: this.state.segundos1 + 1
-          })
-          if(this.state.segundos1 == 10){
-            this.setState({
-              segundos2: this.state.segundos2 + 1,
-              segundos1: 0
-            })
-          }
-          if(this.state.segundos2 == 6){
-                this.setState({
-                  minutos1: this.state.minutos1 + 1,
-                  segundos1: 0,
-                  segundos2: 0,
-                })
-          }
-          if(this.state.minutos1 == 10){
-                  this.setState({
-                    minutos2: this.state.minutos2 + 1,
-                    minutos1: 0
-                  })
-          }
-          if(this.state.minutos2 == 6){
-                this.setState({
-                  horas1: this.state.horas1 + 1,
-                  minutos1: 0,
-                  minutos2: 0,
-                })
-          }
-          if(this.state.horas1 == 10){
-                  this.setState({
-                    horas2: this.state.horas2 + 1,
-                    horas1: 0
-                  })
-          }
-        }, 1000);
+  onLoginChange = (login) => {
+    this.setState({login});
+  }
+  onPasswordChange = (password) => {
+    this.setState({password})
+  }
+  checkUser = () => {
+    if(this.state.password === 'edu102030' && this.state.login == 'Carlos'){
+      alert("Foi!")
+    }
+    else{
+      alert("Não confere")
     }
   }
-
-  timeStart = () => {
-    this.setState({
-      bug: true,
-    })
-    this.time()
-  }
-
-  timeStop = () => {
-    this.setState({
-      segundos1: 0,
-      segundos2: 0,
-      minutos1: 0,
-      minutos2: 0,
-      horas1: 0,
-      horas2: 0,
-      bug: false,
-    })
-    clearInterval(this.Interval)
-  }
-
-  render() {
+  
+  render(){
     return (
-      <View style = {{flex: 100, backgroundColor: '#DCDCDC'}}>
-        <Text style = {{flex: 30, fontSize:60, marginTop: 90, marginLeft: 20}}>Cronômetro</Text>
-        <Text style = {{flex: 20, fontSize:50, marginLeft: 55, marginTop: 40}}>{this.state.horas2}{this.state.horas1} : {this.state.minutos2}{this.state.minutos1} : {this.state.segundos2}{this.state.segundos1}</Text>
-        <View style = {{flex: 50, alignItems: "center"}}>
-          <Button
-            onPress = {this.timeStart}
-            title = "start"
-            color = "#696969"
-          >
-          </Button>
-          <Text/>
-          <Button
-            onPress = {this.timeStop}
-            title = "Stop"
-            color = "#696969"
-          >
-          </Button>
+    <View style = {{flex:100}}>
+      <View style = {{flex:5, flexDirection:'row', backgroundColor:'darkgreen'}}>
+        <Button title = "Voltar" color = "darkgreen">
+        </Button>
+        <View style = {{flex:5, alignItems:'center'}}>
+          <Text style = {{fontSize:20, color:'white'}}>Validador</Text>
         </View>
       </View>
-    )
+      <View style = {{flex:50,flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
+        <Text>Login:</Text>
+        <TextInput style = {{color:'black'}}
+        placeholder = 'Username'
+        onChangeText = {this.onLoginChange}
+        ></TextInput>
+        <Text>Password:</Text>
+        <TextInput style = {{color:'black'}}
+        placeholder = 'Password'
+        onChangeText = {this.onPasswordChange}></TextInput>
+        <Button
+        title = "Submit"color = "black"
+        styles = {styles.button}
+        onPress = {this.checkUser}
+        borderColor = 'white'>
+        </Button> 
+      </View>
+    </View>
+    );
   }
 }
+
+export default App
