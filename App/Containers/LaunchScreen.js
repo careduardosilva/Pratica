6bdd056e192bd82f222f074b18497739b1142f24
careduardosilva <car.eduardosilva@hotmail.com>
2019-03-27 09:20:18 -0300

    Update LaunchScreen.js

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,39 +1,61 @@
 import React, {Component} from 'react';
-import {View, Text, TextInput, Button, YellowBox} from 'react-native';
+import {View, Text, TextInput, Button, TouchableHighlight} from 'react-native';
 import { Directions } from 'react-native-gesture-handler';
-import styles from './Styles/LaunchScreenStyles'
+import {createBottomTabNavigator, createAppContainer} from 'react-navigation';
+import styles from './Styles/LaunchScreenStyles';
+import Feather from 'react-native-vector-icons/Feather';
+import Icon from 'react-native-vector-icons/AntDesign';
 
-class App extends Component {
+class AccountCheck extends Component {
   state = {
     login: '',
     password: '',
+    press_times: 0,
+  }
+  createAccount = () => {
+    this.props.navigation.navigate('Create Account');
   }
   onLoginChange = (login) => {
     this.setState({login});
   }
   onPasswordChange = (password) => {
-    this.setState({password})
+    this.setState({password});
   }
   checkUser = () => {
-    if(this.state.password === 'edu102030' && this.state.login == 'Carlos'){
-      alert("Foi!")
-    }
-    else{
-      alert("Não confere")
+    this.setState({press_times: this.state.press_times + 1})
+    if(this.state.press_times < 4){
+      if(this.state.password === 'edu102030' && this.state.login === 'Carlos'){
+        alert("Foi!")
+      }
+      else{
+        alert("Senha/Login inválido");
+      }
+    } else {
+      alert("Conta bloqueada");
     }
   }
   
   render(){
+    const {navigation} = this.props
     return (
-    <View style = {{flex:100}}>
-      <View style = {{flex:5, flexDirection:'row', backgroundColor:'darkgreen'}}>
-        <Button title = "Voltar" color = "darkgreen">
+    <View style = {styles.v1}>
+      <View style = {{flexDirection:'row', backgroundColor:'darkgreen'}}>
+        <Button title = "Back" color = "darkgreen"
+        icon=
+        {
+          <Icon 
+            name="question"
+            size ={15}
+        />
+        }>
         </Button>
-        <View style = {{flex:5, alignItems:'center'}}>
-          <Text style = {{fontSize:20, color:'white'}}>Validador</Text>
+        <View style = {{flex:20, alignItems:'center'}}>
+          <Text style = {{fontSize:20, color:'white'}}>Access Account</Text>
         </View>
       </View>
-      <View style = {{flex:50,flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
+
+      <View style = {styles.v2}>
+        <View>
         <Text>Login:</Text>
         <TextInput style = {{color:'black'}}
         placeholder = 'Username'
@@ -42,17 +64,62 @@ class App extends Component {
         <Text>Password:</Text>
         <TextInput style = {{color:'black'}}
         placeholder = 'Password'
-        onChangeText = {this.onPasswordChange}></TextInput>
+        onChangeText = {this.onPasswordChange}/>
+        </View>
+        <View style ={{height:100, justifyContent:'space-evenly'}}>
         <Button
-        title = "Submit"color = "black"
-        styles = {styles.button}
-        onPress = {this.checkUser}
-        borderColor = 'white'>
-        </Button> 
+          title = "Submit"color = "darkgreen"
+          styles = {styles.button}
+          onPress = {this.checkUser}
+          borderColor = 'white'/>
+          <Button 
+          title = "Create Account"color = "black"
+          styles = {styles.button}
+          onPress = {this.createAccount}
+          borderColor = 'white'/>
+        </View>
       </View>
+
     </View>
     );
   }
 }
+class CreateAccount extends Component{   
+  render(){
+  return(
+  <View style = {styles.v1}>
+    <View style = {{flexDirection:'row', backgroundColor:'darkgreen'}}>
+        <Button title = "Back" color = "darkgreen">
+        </Button>
+        <View style = {{flex:20, alignItems:'center'}}>
+          <Text style = {{fontSize:20, color:'white'}}>Create Account</Text>
+        </View>
+    </View>
+  </View>
+)
+  }
+}
+const TabNavigator = createBottomTabNavigator(
+  {
+  'Check Account': AccountCheck,
+  'Create Account': CreateAccount,
+  },
+  {
+    defaultNavigationOptions: ({navigation}) => ({
+      tabBarIcon: ({focused,horizontal,tintColor}) => {
+        const{routeName} = navigation.state
+        let iconName;
+        if(routeName == 'Check Account'){
+          iconName = 'user'
+          return <Feather name = {iconName} size = {30} color="900"/>
+        }
+        else if(routeName == 'Create Account'){
+          iconName = 'user-plus'
+          return <Feather name = {iconName} size={30}/>
+        }
+      }
+    })
+  })
+export default createAppContainer(TabNavigator)
 
-export default App
+
